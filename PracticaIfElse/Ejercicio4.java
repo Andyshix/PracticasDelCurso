32549206844dde70f58ec3c17b900e0e8296f30c
Andyshix <you@example.com>
2021-03-31 18:08:07 -0400

    mejora ejercicio 4

--- PracticaIfElse/Ejercicio4.java
+++ PracticaIfElse/Ejercicio4.java
@@ -1,50 +1,36 @@
-    
+
 /**
-* Write a description of class Ejercicio4 here.
-* 
-* @author (Andrea) 
-* @version (1.0)
-*/
+ * Write a description of class Ejercicio4 here.
+ * 
+ * @author (Andrea) 
+ * @version (1.0)
+ */
 public class Ejercicio4
 {
-    public String estadoSem(String estadoSem, int nCambios ){
+    public String estadoSemaforo(String estadoSem, int nCambios ){
         String res = "";
-        while (nCambios >= 1){
-           if(estadoSem == "verde"){
-               estadoSem = "amarillo";
-               nCambios = nCambios - 1;
-           }else {
-               if (estadoSem == "amarillo"){
-                   estadoSem = "rojo";
-                   nCambios  = nCambios - 1;
-               }else {
-                   estadoSem = "verde";
-                   nCambios  = nCambios - 1;
-               }
+        if(nCambios%3==0){
+            res = estadoSem;
+        }else if(nCambios%3==1){
+            if(estadoSem.equals("verde")){
+                estadoSem = "amarillo";
+            }else if (estadoSem.equals("amarillo")){
+                estadoSem = "rojo";
+            }else{
+                estadoSem = "verde";
+            }
+        }else if(nCambios%3==2){
+            if(estadoSem.equals("verde")){
+                estadoSem = "rojo";
+            }else if (estadoSem.equals("amarillo")){
+                estadoSem = "verde";
+            }else {
+                estadoSem = "amarillo";
             }
         }
         res = estadoSem;
         return res;
     }
-   
-    public String estadoSema(String estadoSem, int nCambios ){
-       String res = "";
-       if(nCambios > 0){
-          if(estadoSem == "verde"){
-              estadoSem = "amarillo";
-          }else {
-              if (estadoSem == "amarillo"){
-                 estadoSem = "rojo";
-              }else {
-                 estadoSem = "verde";
-              }
-            }
-          res = estadoSema (estadoSem,nCambios-1);
-       }else{
-          res = estadoSem;
-       }
-       
-       return res;
-    }
-   
+    
+    
 }
